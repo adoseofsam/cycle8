8d46df4247e669b81decfee3bef57da0293bbc64
koolaidrian <chandap2011@gmail.com>
2021-07-31 22:40:57 -0500

    Chanderpaul: added signup functionality

--- EMS/app/forms.py
+++ EMS/app/forms.py
@@ -1,8 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired, Email
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField, PasswordField, SelectField,BooleanField,RadioField,TextAreaField,TextField
+from wtforms.validators import InputRequired,DataRequired, Email, EqualTo
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(),Email()])
     password = PasswordField('Password', validators=[InputRequired()])
+
+class SignUpForm(FlaskForm):
+    
+    role = SelectField('Role', choices = [('Admin','Admin'),('Regular','Regular')]) 
+    FullName = StringField('FullName',validators = [DataRequired()])
+    email = StringField('Email',validators = [DataRequired(),Email()])
+    Profile_Photo = FileField('Profile_photo', validators = [FileRequired(), FileAllowed(['jpg','jpeg','png','Images only!'])])
+    password = PasswordField('Password', validators=[InputRequired(), EqualTo('confirm', message = 'Password must match')])
+    confirm = PasswordField('Password Confirmation')
