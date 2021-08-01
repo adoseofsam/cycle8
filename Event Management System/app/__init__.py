ceb0bb4fa1899a24bb68828e13b8c30dc1cdd3cd
koolaidrian <chandap2011@gmail.com>
2021-07-31 19:53:52 -0500

    chanderpaul: initial push - sketeton

---
+++ Event Management System/app/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+# Config Values
+UPLOAD_FOLDER = './app/static/uploads/'
+USERNAME = 'root'
+PASSWORD = ''
+DATABASE = 'EMS'
+
+# SECRET_KEY is needed for session security, the flash() method in this case stores the message in a session
+SECRET_KEY = 'CodeTitiansSup3r$3cretkey'
+
+
+app = Flask(__name__)
+
+# Flask-SQLAlchemy
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = "mariadb+mariadbconnector://%s:%s@localhost/%s" % (USERNAME,PASSWORD,DATABASE)
+#app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://%s:%s@localhost/%s" % (USERNAME,PASSWORD,DATABASE)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Flask-Login login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+app.config.from_object(__name__)
+from app import views
---
+++ Event Management System/app/models.py
@@ -0,0 +1,57 @@
+from app import db
+from werkzeug.security import generate_password_hash
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    password = db.Column(db.String(128))
+    created_at = db.Column(db.DateTime())
+
+    def __init__(self, email, password, date):
+        self.email = email
+        self.password = password
+        self.created_at = date
+
+
+'''
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(120))
+    email = db.Column(db.String(64), unique=True, index=True)
+    password = db.Column(db.String(120))
+    profile_photo = db.Column(db.String(80))
+    role = db.Column(db.String(20))
+    created_at = db.Column(db.DateTime())
+
+    def __init__(self, full_name, email, password, photo, role, date):
+        self.full_name = full_name
+        self.email = email
+        self.password = generate_password_hash(password, method = 'pbkdf2:sha256')
+        self.profile_photo = photo
+        self.role = role
+        self.created_at = date
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+    
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+'''
