aec78a0f6e2bc9fa056449f94c337e0aa94e019e
TheDryck <48965636+TheDryck@users.noreply.github.com>
2021-08-03 22:23:46 -0500

    Added status code for unauthorised deletion

--- EMS/app/views.py
+++ EMS/app/views.py
@@ -309,7 +309,7 @@ def deleteEvent(id):
             return make_response(jsonify(error = None, message="Event does not exist"), 404)
 
         if current_user.get_role() != "Admin" or current_user.get_role() != "admin":
-            return make_response(jsonify(error = None, message = "You need admin privileges to delete an event"))
+            return make_response(jsonify(error = None, message = "You need admin privileges to delete an event"), 401)
 
         db.session.delete(event)
         db.session.commit()
