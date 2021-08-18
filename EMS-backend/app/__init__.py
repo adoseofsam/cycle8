49f5f18287c24d9ec93cb3acec424063452da0d3
JaviCamp-V <54679525+JaviCamp-V@users.noreply.github.com>
2021-08-18 11:54:52 -0500

    minior changes

--- EMS-backend/app/__init__.py
+++ EMS-backend/app/__init__.py
@@ -2,14 +2,15 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 
 # Config Values
 UPLOAD_FOLDER = './app/static/uploads/'
 
-USERNAME = 'root' #'codeTitan'
-PASSWORD = ''#'password'
-DATABASE = 'emsdb' #'cycle8'
+USERNAME = 'codeTitan'
+PASSWORD = 'password'
+DATABASE = 'cycle8'
 
 # SECRET_KEY is needed for session security, the flash() method in this case stores the message in a session
 SECRET_KEY = 'CodeTitiansSup3r$3cretkey'
@@ -19,14 +20,15 @@ app = Flask(__name__)
 
 # Flask-SQLAlchemy
 app.config['SECRET_KEY'] = SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = "mariadb+mariadbconnector://%s:%s@localhost/%s" % (USERNAME,PASSWORD,DATABASE)
-#app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://%s:%s@localhost/%s" % (USERNAME,PASSWORD,DATABASE)
+#app.config['SQLALCHEMY_DATABASE_URI'] = "mariadb+mariadbconnector://%s:%s@localhost/%s" % (USERNAME,PASSWORD,DATABASE)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://%s:%s@localhost/%s" % (USERNAME,PASSWORD,DATABASE)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # added just to suppress a warning
 
 app.config['WTF_CSRF_ENABLED'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+CORS(app)
 
 # Flask-Login login manager
 login_manager = LoginManager()
